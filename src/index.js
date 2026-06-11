3e63a5cef57d511a0702586f6222e688c8b8cc8f
William <brito.guilherme.jose@gmail.com>
2026-06-11 11:17:04 -0300

    Swagger 1.1
    Atualizando porta

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.use('/api/events', eventRoutes);
 // Rota padrão para servir o Frontend
 app.use(express.static(path.join(__dirname, '../public')));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
